26d89a3d3236d189019cf28010d6f5d80ba2e634
olgakorzhova <olga1584624@gmail.com>
2025-03-02 12:17:31 -0600

    Initialize Spring Boot backend

---
+++ budget-planning-backend/src/test/java/org/launchcode/budget_planning_backend/BudgetPlanningBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.budget_planning_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetPlanningBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
