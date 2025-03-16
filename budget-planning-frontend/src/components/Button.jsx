e97f772c370f5f6301716aaa11a9bad973bf11ed
olgakorzhova <olga1584624@gmail.com>
2025-03-16 15:44:30 -0500

    removed commented lines

--- budget-planning-frontend/src/components/Button.jsx
+++ budget-planning-frontend/src/components/Button.jsx
@@ -2,8 +2,6 @@
 const Button = ({ label, onClick }) => {
     return (
         <button
-            //type="submit"
-            //className={`padding-2 shadow-none hover:shadow background-light-${color} hover:background-dark-${color}`}
             onClick={onClick}>
             {label}
         </button>
