4b0c6db0af4c862c109bf5954581b482d452b99f
Kayalvizhi Rajendran <kayalvizhi.btech@gmail.com>
2025-03-18 18:15:52 -0500

    created form for creating an event, created initial Banner component

--- budget-planning-frontend/src/App.jsx
+++ budget-planning-frontend/src/App.jsx
@@ -1,19 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
+import ChoreCreationForm from './components/ChoreCreationForm';
+import Banner from "./components/Banner"
+import CreateEvent from './components/CreateEvent';
+import Navbar from './components/Navbar'
+import Groups from './Pages/Groups'
+import Events from './Pages/Events'
+import Chores from './Pages/Chores'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <p>Placeholder for the main app page</p>
-
+    <Banner />
+    <CreateEvent />
     </>
 
   )
 }
 
 export default App
+/*<BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/groups" element={<Groups />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/chores" element={<Chores />} />
+      </Routes>
+      </BrowserRouter> */
---
+++ budget-planning-frontend/src/components/CreateEvent.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+
+export default function CreateEvent() {
+  const today = new Date().toISOString().split("T")[0];
+  const [data, setData] = useState({
+    eventName: "",
+    eventBudget: "",
+    eventLocation: "",
+    eventDescription: "",
+    eventDate: "",
+  });
+  const [newErrors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const validateForm = () => {
+    let isValid = true;
+    let newErrors = {};
+    if (
+      !data.eventName.trim() ||
+      data.eventName.length < 3 ||
+      data.eventName.length > 50
+    ) {
+      newErrors.eventName =
+        "Name is required and must be between 3 and 50 characters.";
+      isValid = false;
+    }
+    if (!data.eventBudget || isNaN(data.eventBudget) || data.eventBudget < 0) {
+      newErrors.eventBudget =
+        "Budget is required and must be a positive number";
+      isValid = false;
+    }
+    setErrors(newErrors);
+    alert(isValid);
+    return isValid;
+  };
+
+  const createEvent = (event) => {
+    event.preventDefault();
+   // if (!validateForm()) return;
+
+    fetch("http://localhost:8080/events/create", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    alert("Event Created Successfully");
+  };
+
+  return (
+    <div class="pageBody">
+      <form class="createEventForm">
+        <h3> Create Event</h3>
+        <label for="eventName">Name: </label>
+        <input
+          type="text"
+          id="eventName"
+          name="eventName"
+          onChange={handleChange}
+          setData={setData}
+        ></input>
+        <br></br>
+        {newErrors.eventName && <p className="error">{newErrors.eventName}</p>}
+        <label for="eventBudget">Budget: </label>
+        <input
+          type="number"
+          id="eventBudget"
+          name="eventBudget"
+          onChange={handleChange}
+          setData={setData}
+        ></input>
+        <br></br>
+        {newErrors.eventBudget && (
+          <p className="error">{newErrors.eventBudget}</p>
+        )}
+        <label for="eventLocation">Location: </label>
+        <input
+          type="text"
+          id="eventLocation"
+          name="eventLocation"
+          onChange={handleChange}
+          setDate={setData}
+        ></input>
+        <br />
+        <label for="eventDescription">Description: </label>
+        <textarea
+          id="eventDescription"
+          name="eventDescription"
+          onChange={handleChange}
+          setData={setData}
+        ></textarea>
+        <br />
+        <label for="eventDate">Date: </label>
+        <input
+          type="date"
+          min={today}
+          name="eventDate"
+          onChange={handleChange}
+          setData={setData}
+        />{" "}
+        <br />
+        <button type="submit" label="Create Event" onClick={createEvent}>
+          Create Event
+        </button>
+      </form>
+    </div>
+  );
+}
