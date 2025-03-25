bd6e9987778f0347451e926f205f998dcf955e10
Kayalvizhi Rajendran <kayalvizhi.btech@gmail.com>
2025-03-25 18:02:49 -0500

    removed unused imports

--- budget-planning-frontend/src/App.jsx
+++ budget-planning-frontend/src/App.jsx
@@ -1,22 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Pages/Home";
+import Events from "./components/Pages/Events";
+import Chores from "./components/Pages/Chores";
+import RegisterForm from './components/RegisterForm';
+import LoginForm from './components/LoginForm';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-      <Route path="/Home" element={<Home />} />
-         <Route path="/register" element={<RegisterForm />} />
-                          <Route path="/login" element={<LoginForm />} />
-                          <Route path="/Events" element={<Events />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/Events" element={<Events />} />
         <Route path="/Chores" element={<Chores />} />
       </Routes>
     </>
   );
 }
 
-export default App
+export default App;
--- budget-planning-frontend/src/components/Navbar.jsx
+++ budget-planning-frontend/src/components/Navbar.jsx
@@ -1,34 +1,38 @@
-import React from 'react'
-import { NavLink } from 'react-router'
-import "./Navbar.css"
-import budgetLogo from '../img/logo-transparent-png.png';
-import NavDropdown from "react-bootstrap/NavDropdown";
+import React from "react";
+import { NavLink } from "react-router";
+import "./Navbar.css";
+import budgetLogo from "../img/logo-transparent-png.png";
 
 function Navbar() {
   return (
     <>
-    
-    <div class="nav-bar">  
-      <nav>
-         <img src={budgetLogo} className="logo" alt="Budget Breaker logo" />
-        <NavLink to="/" className='title'> Red, Green, VACAY!
-        </NavLink>
-        <div className='menu'>
+      <div class="nav-bar">
+        <nav>
+          <img src={budgetLogo} className="logo" alt="Budget Breaker logo" />
+          <NavLink to="/" className="title">
+            {" "}
+            Red, Green, VACAY!
+          </NavLink>
+          <div className="menu">
             <span></span>
             <span></span>
             <span></span>
-        </div>
-        <ul>
-            <li><NavLink to="/Home">Home </NavLink></li>
+          </div>
+          <ul>
             <li>
-                <NavLink to="/Events">Events</NavLink>
+              <NavLink to="/Home">Home </NavLink>
             </li>
-            <li><NavLink to="/Chores"> Chores</NavLink></li>
-        </ul>
-      </nav>
-    </div>
+            <li>
+              <NavLink to="/Events">Events</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Chores"> Chores</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
