fe216f3a2fad99475fd841a2d0f65c5d621e0c5e
catcadams <catceciliaadams@gmail.com>
2025-03-31 21:36:08 -0500

    added hardcoded user to automatically add with group creation

--- budget-planning-backend/src/main/java/org/launchcode/budget_planning_backend/controllers/UserGroupController.java
+++ budget-planning-backend/src/main/java/org/launchcode/budget_planning_backend/controllers/UserGroupController.java
@@ -1,5 +1,9 @@
 package org.launchcode.budget_planning_backend.controllers;
 
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpServletRequest;
+import org.launchcode.budget_planning_backend.models.DummyObjectsToBeDeleted;
+import org.launchcode.budget_planning_backend.models.User;
 import org.launchcode.budget_planning_backend.models.UserGroup;
 import org.launchcode.budget_planning_backend.models.dto.UserGroupDTO;
 import org.slf4j.Logger;
@@ -8,6 +12,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import static org.launchcode.budget_planning_backend.models.DummyObjectsToBeDeleted.getUserByID;
 
 @RestController
 @RequestMapping(value="/groups")
@@ -20,11 +27,16 @@ public class UserGroupController {
 
     private final List<UserGroup> groupsByUser = new ArrayList<>();
 
+    private final User user = getUserByID(1);
+
     @PostMapping(value="/create")
     public void postGroups(@RequestBody UserGroupDTO userGroupDTO) {
         UserGroup group = new UserGroup(userGroupDTO.getName(), userGroupDTO.getDescription());
         groupsList.add(group);
-        logger.info("New Group: ".concat(group.toString()));
+        if(user != null && user.getId() == 1) {
+            group.addUsers(user);
+        }
+            logger.info("New Group: ".concat(group.toString()));
     }
 
 }
