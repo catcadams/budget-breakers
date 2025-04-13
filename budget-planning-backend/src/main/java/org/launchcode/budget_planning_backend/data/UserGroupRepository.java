eb8474b260b71a23ce3006d5f92aa71fdfd69878
catcadams <catceciliaadams@gmail.com>
2025-04-13 09:19:29 -0500

    updated usergrouprepository file

--- budget-planning-backend/src/main/java/org/launchcode/budget_planning_backend/data/UserGroupRepository.java
+++ budget-planning-backend/src/main/java/org/launchcode/budget_planning_backend/data/UserGroupRepository.java
@@ -1,9 +1,9 @@
 package org.launchcode.budget_planning_backend.data;
 
-import org.launchcode.budget_planning_backend.models.Group;
+import org.launchcode.budget_planning_backend.models.UserGroup;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GroupRepository extends CrudRepository<Group, Integer> {
+public interface UserGroupRepository extends CrudRepository<UserGroup, Integer> {
 }
