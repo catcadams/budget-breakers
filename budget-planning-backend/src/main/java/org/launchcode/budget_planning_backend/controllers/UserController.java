3b5541f7554896b0416c0f0b2614b92f307c27ac
catcadams <catceciliaadams@gmail.com>
2025-06-12 13:49:31 -0500

    Revert "added controller to access user information"
    This reverts commit 11caf586c57e50352f11b89e2a9f93c71d6b78a7.

--- budget-planning-backend/src/main/java/org/launchcode/budget_planning_backend/controllers/UserController.java
+++ budget-planning-backend/src/main/java/org/launchcode/budget_planning_backend/controllers/UserController.java
@@ -1,44 +1,25 @@
 package org.launchcode.budget_planning_backend.controllers;
-import jakarta.servlet.http.HttpServletRequest;
 import org.launchcode.budget_planning_backend.models.User;
-import org.launchcode.budget_planning_backend.service.UserGroupService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/userAccount")
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+@RequestMapping("/users")
+@CrossOrigin
 public class UserController {
 
-    @Autowired
-    AuthenticationController authenticationController;
+private final List<User> users = new ArrayList<>();
 
-    private final Logger logger = LoggerFactory.getLogger(UserController.class);
-
-//private final List<User> users = new ArrayList<>();
-
-    @GetMapping(value = "/{userID}")
-    public ResponseEntity<User> displayUserAccountPage (HttpServletRequest request) {
-        User currentUser = authenticationController.getUserFromSession(request.getSession());
-
-        if (currentUser == null) {
-            logger.info("User Not Found");
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
-        logger.info("User ID: " + currentUser.getId());
-        return ResponseEntity.ok(currentUser);
+    @GetMapping
+    public List<User> getUsers() {
+        return users;
     }
 
-//    @PostMapping
-//    public List<User> addUser(@RequestBody User newUser) {
-//        users.add(newUser);
-//        return users;
-//    }
+    @PostMapping
+    public List<User> addUser(@RequestBody User newUser) {
+        users.add(newUser);
+        return users;
+    }
 }
